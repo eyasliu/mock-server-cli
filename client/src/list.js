daf1480e4771c74737885e0e836751a72b3a47c3
eyas <liuyuesongde@163.com>
2016-12-15 21:01:48 +0800

    change preview domain

--- client/src/list.js
+++ client/src/list.js
@@ -210,11 +210,17 @@ export default class ProjectList extends PureComponent{
 		newApis[url] = data
 		this.setState({apis: newApis})
 	}
+	preview(key){
+		const previewDomain = "http://eyasweb.com/mock-request/"
+		const parse = key.split(' ')
+		
+		return previewDomain + '?submit&path=' + location.origin + (parse[1] ? parse[1] + '&method=' + parse[0] : parse[0])
+		
+	}
 	openApi(key, e){
 		e.preventDefault();
 		e.stopPropagation();
-		const parse = key.split(' ')
-		window.open(parse[(parse.length - 1)])
+		window.open(this.preview(key))
 	}
 	searchFilter(apis){
 		const keyword = this.state.searchKeyword
